deaffa0b5587acce33c22b0a3c2872bdbc7b4292
benxh7 <banjaminmorales571@gmail.com>
2024-09-13 19:35:32 -0300

    Actividades 1 y 2: Ejercicio sobre arreglos

--- Arreglos de una Dimension/Actividad2_S7_Ejercicios_Sobre_Arreglos.py
+++ Arreglos de una Dimension/Actividad2_S7_Ejercicios_Sobre_Arreglos.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-matrix = np.zeros((3, 3)) # Crear un arreglo 3x3 con ceros
-np.fill_diagonal(matrix, [1, 2, 3]) # Asignar los valores 1, 2 y 3 a la diagonal principal
+matrix = np.diag([1, 2, 3]) # Crear una matriz diagonal con los valores 1, 2, 3
 
 print("Matriz con ceros y diagonal principal con 1, 2, 3:")
 print(matrix)
